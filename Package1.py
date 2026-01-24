4c9570808b6b33ce87dc33f10c8837aaa4a344d9
Akash Chatterjee <akashthecreater1@gmail.com>
2026-01-25 04:02:29 +0530

    Update Package1.py

--- Package1.py
+++ Package1.py
@@ -1,16 +1,8 @@
-#-------------------------------------------------------------------------------
-# Name:        module2
-# Purpose:
-#
-# Author:      AKASH CHATTERJEE
-#
-# Created:     01-12-2025
-# Copyright:   (c) AKASH CHATTERJEE 2025
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+
 
 def main():
     pass
 
 if __name__ == '__main__':
     main()
+
